3a1b65e97826893cda052aa85108e6d5ae0006d7
oliverduce@googlemail.com <oliverduce@googlemail.com>
2024-09-24 07:04:59 +0000

    Created Banner, Form1_copy, Form2, Main and 1 more; Edited settings

---
+++ client_code/Main/__init__.py
@@ -0,0 +1,34 @@
+from ._anvil_designer import MainTemplate
+from anvil import *
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+
+class Main(MainTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Check if a user is already logged in
+    user = anvil.users.get_user(allow_remembered=True)
+
+    if user:
+      # User is already logged in
+      alert(f"Welcome back, {user['email']}")
+
+    else:
+      # User is not logged in; prompt login
+      self.label_login.text = "Logged-out"
+      anvil.users.login_with_form()
+
+    self.set_logged_in_user()
+
+  def set_logged_in_user(self):
+    user = anvil.users.get_user(allow_remembered=True)
+    alert("Welcome!")
+    self.label_login.text = f"Logged in as {user['email']}"
