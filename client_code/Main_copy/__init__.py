47b51e2024dd98dc915c4b236f853ea2e8d73eef
oliverduce@googlemail.com <oliverduce@googlemail.com>
2024-09-25 14:22:49 +0000

    Created Main_copy; Edited Main and header; Deleted Banner, Banner_copy, Form1, Main_Copy and 5 more

--- client_code/Main_copy/__init__.py
+++ client_code/Main_copy/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Main_CopyTemplate
+from ._anvil_designer import Main_copyTemplate
 from anvil import *
 import anvil.facebook.auth
 import anvil.google.auth, anvil.google.drive
@@ -9,29 +9,26 @@ from anvil.tables import app_tables
 import anvil.users
 
 
-class Main_Copy(Main_CopyTemplate):
+class Main_copy(Main_copyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+
     # Check if a user is already logged in
     user = anvil.users.get_user(allow_remembered=True)
-    
+
     if user:
       # User is already logged in
       alert(f"Welcome back, {user['email']}")
-     
+
     else:
       # User is not logged in; prompt login
       self.label_login.text = "Logged-out"
       anvil.users.login_with_form()
-    
+
     self.set_logged_in_user()
-    
 
   def set_logged_in_user(self):
     user = anvil.users.get_user(allow_remembered=True)
     alert("Welcome!")
     self.label_login.text = f"Logged in as {user['email']}"
-
-  
