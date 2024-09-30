b493f7a647f49941612020060b37be725ee63525
oliverduce@googlemail.com <oliverduce@googlemail.com>
2024-09-30 13:08:12 +0000

    gonna delete main copy 1

--- client_code/Main_copy/__init__.py
+++ client_code/Main_copy/__init__.py
@@ -13,7 +13,6 @@ class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Check if a user is already logged in
     user = anvil.users.get_user(allow_remembered=True)
 
@@ -26,7 +25,13 @@ class Main(MainTemplate):
       self.label_login.text = "Logged-out"
       anvil.users.login_with_form()
 
+    rows = app_tables.reports.search()
+    self.repeating_panel_1.items = rows
+    print(rows)# Bind rows to the Repeating Panel
    
+
+
+  
   def set_logged_in_user(self):
     user = anvil.users.get_user(allow_remembered=True)
     self.label_login.text = f"Logged in as {user['email']}"
