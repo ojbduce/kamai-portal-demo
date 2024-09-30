efdeb242fdccb830f97f681541831e197b166697
oliverduce@googlemail.com <oliverduce@googlemail.com>
2024-09-30 14:40:45 +0000

    Back with new main publish

--- client_code/Main_Copy/__init__.py
+++ client_code/Main_Copy/__init__.py
@@ -30,39 +30,29 @@ class Main_Copy(Main_CopyTemplate):
 
     self.get_data()
 
-    def get_data(self):
-      rows = app_tables.reports.search()
-      self.repeating_panel_1.items = rows
-      print(rows)# Bind rows to the Repeating Panel
-   
-
-
-  
-  def set_logged_in_user(self):
-    user = anvil.users.get_user(allow_remembered=True)
-    self.label_login.text = f"Logged in as {user['email']}"
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    # Check if a user is already logged in
-    user = anvil.users.get_user(allow_remembered=True)
-
-    if user:
-      # User is already logged in
-       self.set_logged_in_user()
-
-    else:
-      # User is not logged in; prompt login
-      self.label_login.text = "Logged-out"
-      anvil.users.login_with_form()
-
+  def get_data(self):
     rows = app_tables.reports.search()
     self.repeating_panel_1.items = rows
     print(rows)# Bind rows to the Repeating Panel
    
 
-
-  
   def set_logged_in_user(self):
     user = anvil.users.get_user(allow_remembered=True)
     self.label_login.text = f"Logged in as {user['email']}"
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel_home.visible = False
+    self.card_database.visible = True
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('header')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Coming soon!")
+
+
+  
+ 
