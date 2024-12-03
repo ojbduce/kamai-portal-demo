ff37772fc04340565386afd5a05adee2854fc86d
oliverduce@googlemail.com <oliverduce@googlemail.com>
2024-12-03 13:26:34 +0000

    Edited Yoti_QR_Login

--- client_code/Yoti_QR_Login/__init__.py
+++ client_code/Yoti_QR_Login/__init__.py
@@ -38,4 +38,4 @@ class Yoti_QR_Login(Yoti_QR_LoginTemplate):
 
   def button_home_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('Main_Copy')
+    open_form('Home')
