ef4c75fa6312c95010031eb789de6789ab64b1ac
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-01-18 10:56:43 +0000

    Edited Main, Main.RowTemplate1 and settings

--- client_code/Main/RowTemplate1/__init__.py
+++ client_code/Main/RowTemplate1/__init__.py
@@ -15,5 +15,6 @@ class RowTemplate1(RowTemplate1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
+    
 
     # Any code you write here will run before the form opens.
