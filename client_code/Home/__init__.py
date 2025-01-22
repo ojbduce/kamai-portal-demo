4761c0c8a757afe6833e924b5f8224f0874d2b5c
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-01-22 16:45:45 +0000

    Login Design improvement and set startup to login

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -19,9 +19,6 @@ class Home(HomeTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def form_show(self, **event_args):
-    """This method is called when the form is shown on the page"""
-    pass
-
+ 
   
     
