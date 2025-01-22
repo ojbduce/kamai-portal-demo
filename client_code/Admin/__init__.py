45392ad64bce83725494939fcc3cf8717f082978
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-01-22 11:14:44 +0000

    Created Admin; Edited settings and Data_View; Deleted Form2 and Form1

--- client_code/Admin/__init__.py
+++ client_code/Admin/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import AdminTemplate
 from anvil import *
 import anvil.server
 import anvil.facebook.auth
@@ -10,7 +10,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class Admin(AdminTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
