f546a5397f503bbe19248fe9c91e8fe054e064e4
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-06-18 06:04:58 +0000

    Created digital_leaders_image background.png; Edited Arrive

--- client_code/Arrive/__init__.py
+++ client_code/Arrive/__init__.py
@@ -17,7 +17,7 @@ class Arrive(ArriveTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.js.get_dom_node()
+    # anvil.js.get_dom_node()
 
     # Any code you write here will run before the form opens.
 
