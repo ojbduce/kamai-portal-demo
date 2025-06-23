a8c8dff72fe041421908946c8cb62ce6b2e640bc
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-06-23 07:24:04 +0000

    Edited settings and Main_Blank; Deleted custom and test

--- client_code/Main_Blank/__init__.py
+++ client_code/Main_Blank/__init__.py
@@ -11,7 +11,188 @@ from anvil.tables import app_tables
 
 class Main_Blank(Main_BlankTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    # Set Form pfrom ._anvil_designer import MainTemplate
     self.init_components(**properties)
+    self.timer_1.interval = 0
+    self.remember_me_id = ''
+    print(f"self.remember_me_id is {self.remember_me_id}")
+    self.yoti_loggin_in_box.visible = False #MOVE UI SET UP TO OWN METHOD?
+    self.button_show_data.visible = False
+    self.outlined_card_digi_leaders.visible = True
+    self.card_database.visible = False 
+    self.label_title.visible = True
+    # anvil.server.call('hello')
+    anvil.js.window.console.log("Kaimai Home Page. Logging in User")
+    try:
+      anvil.js.window.console.log("Logging In USER")
+      print("Logging in User")
+      user = anvil.users.get_user(allow_remembered=True) #we want to be using the Usets Service expliticitly.
+      if user:
+        anvil.js.window.console.log("Client has found Existing User")
+        print(f"Client has found Existing user {user['remember_me_id']}")
+        self.remember_me_id = user['remember_me_id']
+        print("self.remember_me_id variable is set")
+        self.show_yoti_logged_in_box()
+        # alert("Logged-in in Admin Mode")
+        # self.button_show_data.visible = True
+        anvil.server.call('log_in_embedded_app', self.remember_me_id)
+      else:
+        print("No Yoti User found")
+      # self.show_yoti_logged_in_box()
+      if user is None:
+        try:
+          print("No Anvil Users Service User. Getting Url_Hash") #OK
+          self.remember_me_id = get_url_hash()
+          print(f"remember_me_id from get_url_hash is: {self.remember_me_id}")
+          user = app_tables.users.get(remember_me_id=self.remember_me_id) #move server side?
+          if user:
+            logged_in = anvil.server.call('force_login', user)
+            alert(logged_in)
+            print(logged_in)
+        except Exception as e:
+          anvil.js.window.console.log("Error anvil,users force login {e}.")
+        try:
+          #Slightly redundant this fallback as we have a synthetic loin button option from QR_login
+          #user = anvil.server.call('fall_back_user') # won't call so try separate func
+          print("Using fall back user")
+          user,self.remember_me_id = anvil.server.call('fall_back_user')
+          print('remember_me_id')
+          print(f"fallback_id {user['remember_me_id]']}")#was fine now doesn't call
+          anvil.js.window.console.log("Fallback User")
+          print(user)
+          if user:
+            print(f" User logged-in: {user['email']}")
+            self.remember_me_id = user['remember_me_id']
+            print('self.remember_me_id is set')
+            # alert("Logged-in in Admin Mode")
+            # Notification("Logged-in in Admin Mode")
+            # self.button_show_data.visible = True
+            self.show_yoti_logged_in_box()
+          else:
+            anvil.js.window.console.log("Reverting to Login Form")
+            anvil.users.login_with_form(allow_remembered=True)
+        except Exception as e:
+          anvil.js.window.console.log("Error anvil,users.get_user generating {e} null type.User not found Client side")
+    except Exception as e:
+      anvil.js.window.console.log("Error anvil,users.get_user generating {e} null type.User not found Client side")
 
-    # Any code you write here will run before the form opens.
+  # def use_fallback_user(self):
+  #   print("Hit fallback user func")
+  #   fall_back_user = anvil.server.call('fall_back_user')
+
+
+
+  def link_digi_leaders_image_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    # url = 'https://adept-right-category.anvil.app' #users does not work
+    # self.link_digi_leaders_image.url =url
+    url = 'https://adept-right-category.anvil.app/#?remember_me_id=' + self.remember_me_id
+    print(f"URL is {url}")
+    self.link_digi_leaders_image.url =url
+
+
+  def show_yoti_logged_in_box(self):
+    print("Hit show_yoti_login func")
+    self.yoti_loggin_in_box.visible = True
+    self.yoti_loggin_in_box.tooltip = "Logged in with Yoti"
+    # self.button_show_data.visible = True
+
+  def get_data(self):
+    rows = app_tables.reports.search()
+    self.repeating_panel_1.items = rows
+    #print(rows)# Bind rows to the Repeating Panel
+
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    current_remember_me_id = self.remember_me_id
+    app_tables.reports.add_row(current_remember_me_id=current_remember_me_id)
+    self.get_data()
+    self.outlined_card_digi_leaders.visible = False
+    self.card_database.visible = True
+    self.label_title.visible = False
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('header')
+
+  # def url_test_button_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   share_url = anvil.server.call('yoti_session')
+  #   self.label_return_message.visible = True
+  #   self.label_return_message.text = share_url
+  #   if share_url == "Keys not found":
+  #     Notification("Keys not found")
+  #   elif share_url == "Error creating share session.":
+  #     Notification("Error creating share session.")
+  #   else:
+  #     Notification("Share_Url test passed")
+
+
+  # def button_test_bd_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   self.name = 'test_444'
+  #   app_tables.files.add_row(name = self.name)
+  #   print("Added file to files table")
+  #   for row in app_tables.files.search():
+  #     print(row['name'])
+
+
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    alert("Next...")
+
+  # def link_2_copy_click(self, **event_args):
+  #   """This method is called when the link is clicked"""
+  #   pass
+
+  def button_show_data_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # self.get_data()
+    # self.outlined_card_digi_leaders.visible = False
+    # self.card_database.visible = True
+    # self.label_title.visible = False
+    open_form('Admin')
+
+  def button_about_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+
+
+
+# def button_1_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   anvil.server.call('check_origin')
+
+  # def button_server_session_test_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   anvil.server.call('server_session_data')
+
+  # def button_cookies_click(self, **event_args):
+  #   anvil.server.call('cookies')
+  #   anvil.server.call('server_session_data')
+  #   """This method is called when the button is clicked"""
+
+  # def form_show(self, **event_args):
+  #   """This method is called when the form is shown on the page"""
+  #   self.remember_me_id = anvil.server.call('get_user')
+  #   print(f"On Form Show Event self.remember_me_id i: {self.remember_me_id}")
+
+  # def form_show(self, **event_args):
+  #   """This method is called when the form is shown on the page"""
+  #   if anvil.designer.in_designer:
+  #     return
+  #   self.button_show_data.visible=False
+  #   alert("Logged-in in Admin Mode")
+  #   self.button_show_data.visible = True
+
+
+
+
+
+
+
+
+
