09230c6a42010d170c616714fae2a8f4d6e853ec
oliverduce@googlemail.com <oliverduce@googlemail.com>
2025-09-19 10:27:02 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -16,20 +16,20 @@ class Form1(Form1Template):
     self.init_components(**properties)
     # inside __init__
     self.home_btn = Button(text="Home")
-    self.admin_btn = Button(text="Admin")
-    self.about_btn = Button(text="About")
-    self.login_btn = Button(text="Login")
-    self.login_status_lbl = Label(text="Not logged in", visible=False, role="caption")
+    # self.admin_btn = Button(text="Admin")
+    # self.about_btn = Button(text="About")
+    # self.login_btn = Button(text="Login")
+    # self.login_status_lbl = Label(text="Not logged in", visible=False, role="caption")
     
-    self.add_component(self.home_btn, slot='home_btn_slot')
-    self.add_component(self.admin_btn, slot='admin_btn_slot')
-    self.add_component(self.about_btn, slot='about_btn_slot')
-    self.add_component(self.login_btn, slot='login_btn_slot')
-    self.add_component(self.login_status_lbl, slot='login_status_slot')
+    self.add_component(self.home_btn) 
+    # self.add_component(self.admin_btn, slot='admin_btn_slot')
+    # self.add_component(self.about_btn, slot='about_btn_slot')
+    # self.add_component(self.login_btn, slot='login_btn_slot')
+    # self.add_component(self.login_status_lbl, slot='login_status_slot')
     
     self.home_btn.set_event_handler('click', self.home_click)
-    self.admin_btn.set_event_handler('click', self.admin_click)
-    self.about_btn.set_event_handler('click', self.about_click)
-    self.login_btn.set_event_handler('click', self.login_click)
+    # self.admin_btn.set_event_handler('click', self.admin_click)
+    # self.about_btn.set_event_handler('click', self.about_click)
+    # self.login_btn.set_event_handler('click', self.login_click)
     
         # Any code you write here will run before the form opens.
